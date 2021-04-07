9651336d7e50d3e3dfaa5138f15f28c438e139a0
prabhav-pandya <business.prabhav@gmail.com>
2021-04-07 18:41:56 +0530

    added timestamp

--- src/medops/Main.java
+++ src/medops/Main.java
@@ -2,9 +2,14 @@ package medops;
 
 import medops.screens.Window;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//        LocalDateTime now = LocalDateTime.now();
+//        System.out.println(dtf.format(now));
         Window medops = new Window();
     }
 }
