4b04ca972eb7514c62be8f07f176bc220093ffc4
prabhav-pandya <business.prabhav@gmail.com>
2021-04-08 14:03:01 +0530

    fix thish plis

--- src/medops/screens/InventoryPanel.java
+++ src/medops/screens/InventoryPanel.java
@@ -1,10 +1,23 @@
 package medops.screens;
 
+import medops.Medicine;
+
 import javax.swing.*;
 
 public class InventoryPanel {
     public JPanel inventoryPanel;
     private JList list1;
+    private DefaultListModel<Medicine> tempList;
     private JTextArea medicineDetailsTextArea;
     private JButton updateButton;
+
+    public InventoryPanel(){
+        tempList = new DefaultListModel<>();
+        for (int i = 0; i < EmployeeScreen.storeRecord.medicineList.size(); i++) {
+            Medicine medicine = EmployeeScreen.storeRecord.medicineList.get(i);
+            tempList.addElement(medicine);
+        }
+        list1 = new JList(tempList);
+    }
+
 }
